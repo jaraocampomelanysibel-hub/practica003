d1a4ce85fbd8491492f19d8d8b2dc7e989ce178e
jaraocampomelanysibel-hub <jaraocampomelanysibel@cetis50cdmx.com>
2025-09-17 19:11:43 -0600

    Add files via upload

---
+++ import flet as ft.py
@@ -0,0 +1,34 @@
+import flet as ft 
+import math
+
+
+def main(page: ft.Page):
+    page.title="Calculadora Simple"
+    page.bgcolor=ft.Colors.PURPLE_ACCENT_400
+
+    titulo=ft.Text(
+        "📕 Calculadora Basica",
+        size=28,
+        text_aling="center",
+        weight="bold"
+    )
+
+    num1=ft.TextField(
+        label="Numero 1",
+        width=200,
+        text_style=ft.TextStyle(color=ft.Colors.GREEN)
+    )
+   
+    num2=ft.TextField(
+        label="Numero 2",
+        width=200,
+        text_style=ft.TextStyle(color=ft.Colors.GREEN)
+    )
+
+    resultado=ft.Text(
+        value="Resultado",
+        size=20,
+        color=ft.Colors.GREEN,
+        text_aling="center"
+    )
+    
